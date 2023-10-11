9abaf79898dca0a1e233271a06f08c0f87c7c128
rsandhu04 <rkaursandhu04@gmail.com>
2023-10-11 01:09:26 +0100

    css changes

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function biggerImg(x)
+	{
+	x.style.height="250%";
+	x.style.width="280%";
+	document.getElementById("myname").style.left = "20%";
+	document.getElementById("myname").style.top = "5%";
+
+	}
+	
+function originalImg(x)
+	{
+	x.style.height="410%";
+	x.style.width="300%";
+	document.getElementById("myimage").style.left = "20%";
+	document.getElementById("myimage").style.top = "50%";
+
+	}
+	
